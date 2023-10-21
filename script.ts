ed36436a79a61bc4917a31ba95e728bc9b27c8b3
mike242424 <mikebyers24@gmail.com>
2023-10-21 13:42:17 -0400

    create a new user record with a post record

--- script.ts
+++ script.ts
@@ -1,18 +1,28 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 async function main() {
-  const users = await prisma.user.findMany();
-  console.log(users);
+  const user = await prisma.user.create({
+    data: {
+      name: "Bob",
+      email: "bob@prisma.io",
+      posts: {
+        create: {
+          title: "Hello World",
+        },
+      },
+    },
+  });
+  console.log(user);
 }
 
 main()
   .then(async () => {
-    await prisma.$disconnect()
+    await prisma.$disconnect();
   })
   .catch(async (err) => {
-    console.error(err)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
+    console.error(err);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
