8d4bdfbf666bb30631fa789bcd1462aad4f57b00
mike242424 <mikebyers24@gmail.com>
2023-10-21 13:39:02 -0400

    create new user record

---
+++ script.ts
@@ -0,0 +1,23 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const user = await prisma.user.create({
+    data: {
+      name: "Alice",
+      email: "alice@prisma.io",
+    },
+  });
+  console.log(user);
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (err) => {
+    console.error(err);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
